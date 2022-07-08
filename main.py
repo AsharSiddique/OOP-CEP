a072a35bf0d16ab76bcf94002e64ae37fedabec2
Ashar <ASHAR4404293@cloud.neduet.edu.pk>
2022-07-08 11:32:44 -0700

    changed a line

--- main.py
+++ main.py
@@ -11,6 +11,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__mai
-    print_hi('PyCharm')
-
+    print_hi('PyCharm
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
